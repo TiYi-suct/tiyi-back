f7477a14ef3e1d22a9d75661ce29abf4b607969d
maskira <361299572@qq.com>
2024-06-09 22:50:10 +0800

    feat: 移调接口

--- controller/analysis_controller.py
+++ controller/analysis_controller.py
@@ -5,7 +5,7 @@ from flask import request
 from flask_restful import Resource
 
 from interceptor.interceptor import login_required
-from service.analysis_service import mel_spectrogram, spectrogram, bpm
+from service.analysis_service import mel_spectrogram, spectrogram, bpm, transposition
 
 
 # 梅尔频谱图
@@ -28,6 +28,7 @@ class Spectrogram(Resource):
         return spectrogram(audio_id, float(start_time), float(end_time))
 
 
+# BPM
 class BPM(Resource):
     @login_required
     def get(self):
@@ -35,3 +36,14 @@ class BPM(Resource):
         start_time = request.args.get('start_time', 0)
         end_time = request.args.get('end_time', -1.0)
         return bpm(audio_id, float(start_time), float(end_time))
+
+
+# 移调
+class Transposition(Resource):
+    @login_required
+    def get(self):
+        audio_id = request.args.get('audio_id')
+        start_time = request.args.get('start_time', 0)
+        end_time = request.args.get('end_time', -1.0)
+        n_steps = request.args.get('n_steps', 0)
+        return transposition(audio_id, float(start_time), float(end_time), int(n_steps))
